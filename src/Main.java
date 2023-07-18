21c38c0b99d4f0164810a3c23624d5590a18965b
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-18 14:54:42 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // (A^B)%C
+        int answer=1;
+        for(int i=0; i<b; i++){
+            answer*=a;
+            answer%=c;
+        }
+
+        System.out.println(answer);
+    }
+}
